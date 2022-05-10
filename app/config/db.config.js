ef4ac4262f40a1a8f48e183eaa1d8adde2c6cf47
fdadalto <ffdadalto@gmail.com>
2022-05-10 01:36:17 -0300

    Banco

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,18 @@
-// Casa
-module.exports = {
-    HOST: "localhost", // Ip ou nome do host onde está o SQL Server
-    PORT: "1433", // Porta utilizada no SQL Server        
-    USER: "sa", // Usuario
-    PASSWORD: "YKcj10bc**", // Senha
-    DB: "BobsBurger", // Banco ao qual se deseja conectar
-    dialect: "mssql", // Tipo de Servidor de Banco de dados (SQL Server, Postgres, mySQL)
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
+// // Casa
+// module.exports = {
+//     HOST: "localhost", // Ip ou nome do host onde está o SQL Server
+//     PORT: "1433", // Porta utilizada no SQL Server        
+//     USER: "sa", // Usuario
+//     PASSWORD: "YKcj10bc**", // Senha
+//     DB: "BobsBurger", // Banco ao qual se deseja conectar
+//     dialect: "mssql", // Tipo de Servidor de Banco de dados (SQL Server, Postgres, mySQL)
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
 
 // module.exports = {
 //     HOST: "192.168.10.64", // Ip ou nome do host onde está o SQL Server
@@ -30,18 +30,18 @@ module.exports = {
 // };
 
 
-// // Amazon
-// module.exports = {
-//     HOST: "54.207.178.130", // Ip ou nome do host onde está o SQL Server
-//     PORT: "1433", // Porta utilizada no SQL Server        
-//     USER: "sa", // Usuario
-//     PASSWORD: "YKcj10bc**", // Senha
-//     DB: "BobsBurger", // Banco ao qual se deseja conectar
-//     dialect: "mssql", // Tipo de Servidor de Banco de dados (SQL Server, Postgres, mySQL)
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
+// Amazon
+module.exports = {
+    HOST: "54.233.225.47", // Ip ou nome do host onde está o SQL Server
+    PORT: "1433", // Porta utilizada no SQL Server        
+    USER: "sa", // Usuario
+    PASSWORD: "YKcj10bc**", // Senha
+    DB: "BobsBurger", // Banco ao qual se deseja conectar
+    dialect: "mssql", // Tipo de Servidor de Banco de dados (SQL Server, Postgres, mySQL)
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
